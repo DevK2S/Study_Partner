ac41f2d9a137c770e577e131fa0aa66637376ec3
Saket5 <41209802+Saket5@users.noreply.github.com>
2020-07-06 18:13:47 +0530

    Update MainActivity.java

--- app/src/main/java/com/studypartner/MainActivity.java
+++ app/src/main/java/com/studypartner/MainActivity.java
@@ -1,14 +1,39 @@
 package com.studypartner;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+				101);
+	}
+
+	public void checkPermission(String permission, int requestCode) {
+		if (ContextCompat.checkSelfPermission(MainActivity.this, permission)
+				== PackageManager.PERMISSION_DENIED) {
+
+			// Requesting the permission
+			ActivityCompat.requestPermissions(MainActivity.this,
+					new String[]{permission},
+					requestCode);
+		}
+		/*else {
+			Toast.makeText(MainActivity.this,
+					"Permission already granted",
+					Toast.LENGTH_SHORT)
+					.show();
+		}*/
+
 	}
 }
