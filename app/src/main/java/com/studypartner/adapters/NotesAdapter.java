d2fa3d7cbb297e0198d324b79120d0d784c40eb4
Saket5 <41209802+Saket5@users.noreply.github.com>
2020-07-16 15:24:40 +0530

    Notes:add,,delete alpha

--- app/src/main/java/com/studypartner/adapters/NotesAdapter.java
+++ app/src/main/java/com/studypartner/adapters/NotesAdapter.java
@@ -1,13 +1,16 @@
 package com.studypartner.adapters;
 
 import android.content.Context;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.studypartner.R;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
@@ -16,9 +19,9 @@ import androidx.recyclerview.widget.RecyclerView;
 public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHolder> {
     
     private Context context;
-    private ArrayList<String> list;
+    private ArrayList<Pair<String,String> >list;
     
-    public NotesAdapter(Context context, ArrayList<String> list) {
+    public NotesAdapter(Context context,ArrayList<Pair<String,String> >list) {
         this.context = context;
         this.list = list;
     }
@@ -32,8 +35,22 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHol
     }
     
     @Override
-    public void onBindViewHolder(@NonNull NotesViewHolder holder, int position) {
-        holder.folderName.setText(list.get(position));
+    public void onBindViewHolder(@NonNull NotesViewHolder holder, final int position) {
+        holder.folderName.setText(list.get(position).second);
+        holder.deleteButton.setText("Delete");
+        holder.deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //notifyDataSetChanged();
+                notifyItemRemoved(position);
+                notifyItemRangeChanged(position, list.size());
+                File file = new File(list.get(position).first);
+                list.remove(position);
+                file.delete();
+
+            }
+        });
     }
     
     @Override
@@ -44,10 +61,11 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHol
     static class NotesViewHolder extends RecyclerView.ViewHolder {
         
         TextView folderName;
-        
+        Button deleteButton;
         public NotesViewHolder(View view) {
             super(view);
-            folderName = view.findViewById(R.id.text1);
+            folderName = view.findViewById(R.id.folder_name);
+            deleteButton=view.findViewById(R.id.delete_button);
         }
     }
 }
--- app/src/main/java/com/studypartner/fragments/NotesFragment.java
+++ app/src/main/java/com/studypartner/fragments/NotesFragment.java
@@ -2,10 +2,13 @@ package com.studypartner.fragments;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.studypartner.R;
 import com.studypartner.activities.MainActivity;
 import com.studypartner.adapters.NotesAdapter;
@@ -15,14 +18,16 @@ import java.util.ArrayList;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class NotesFragment extends Fragment {
 	private static final String TAG = "NotesFragment";
-	
+	private FloatingActionButton fab;
 	private RecyclerView recyclerView;
-	
+	private String path;
+	private File noteFolder;
 	public NotesFragment() {
 	}
 	
@@ -32,7 +37,8 @@ public class NotesFragment extends Fragment {
 		Log.d(TAG, "onCreateView: starts");
 		
 		View rootView = inflater.inflate(R.layout.fragment_notes, container, false);
-		
+		noteFolder=new File(String.valueOf(getContext().getExternalFilesDir(null)), "Folders");
+
 		final MainActivity activity = (MainActivity) requireActivity();
 		activity.mBottomAppBar.bringToFront();
 		activity.fab.bringToFront();
@@ -44,14 +50,29 @@ public class NotesFragment extends Fragment {
 				activity.mNavController.navigate(R.id.action_nav_notes_to_nav_home);
 			}
 		});
-		
+
+		fab = activity.fab;
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(activity, "hehe maachuda", Toast.LENGTH_SHORT).show();
+				addFolder();
+				displayFolder();
+			}
+		});
 		recyclerView = rootView.findViewById(R.id.recyclerView);
 		
 		displayFolder();
 		
 		return rootView;
 	}
-	
+	void addFolder()
+	{
+		String newFolder="New Folder";
+		File file=new File(newFolder,newFolder);
+		file.mkdirs();
+
+	}
 	void displayFolder() {
 		
 		File file = new File(String.valueOf(getContext().getExternalFilesDir(null)), "Folders");
@@ -59,7 +80,6 @@ public class NotesFragment extends Fragment {
 		File test2 = new File(String.valueOf(getContext().getExternalFilesDir(null)), "Folders/test2");
 		File test3 = new File(String.valueOf(getContext().getFilesDir()), "test3");
 		File test4 = new File(String.valueOf(getContext().getExternalMediaDirs()[0]), "test4");
-		
 		test.mkdirs();
 		
 		test2.mkdirs();
@@ -69,14 +89,13 @@ public class NotesFragment extends Fragment {
 		test4.mkdirs();
 		
 		File[] f = file.listFiles();
-		
-		ArrayList<String> subjects = new ArrayList<>();
+		ArrayList<Pair<String,String>> subjects = new ArrayList<>();
 		
 		if (f != null && f.length > 0) {
 			
-			for (File value : f) subjects.add(value.getName());
-			subjects.add(test3.getName());
-			subjects.add(test4.getName());
+			for (File value : f) subjects.add(new Pair(value.getAbsolutePath(),value.getName()));
+			subjects.add(new Pair(test3.getAbsolutePath(),test3.getName()));
+			subjects.add(new Pair(test4.getAbsolutePath(),test4.getName()));
 			
 			NotesAdapter adapter = new NotesAdapter(getContext(), subjects);
 			recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
