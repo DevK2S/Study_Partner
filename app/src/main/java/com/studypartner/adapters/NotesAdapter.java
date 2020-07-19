483910b159de250601024186cd017ad5d039aa2b
krayong <kgourisaria2001@gmail.com>
2020-07-19 15:33:29 +0530

    Folders within Folders
    Signed-off-by: krayong <kgourisaria2001@gmail.com>

--- app/src/main/java/com/studypartner/adapters/NotesAdapter.java
+++ app/src/main/java/com/studypartner/adapters/NotesAdapter.java
@@ -1,87 +1,114 @@
 package com.studypartner.adapters;
 
 import android.content.Context;
-import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.studypartner.R;
-import com.studypartner.activities.MainActivity;
 import com.studypartner.models.FileItem;
 
-import java.io.File;
 import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHolder> {
+	private static final String TAG = "NotesAdapter";
 	
-	private Context context;
-	private ArrayList<FileItem> list;
-	MainActivity activity;
+	public interface NotesClickListener {
+		void onClick(View view, int position);
+		void onLongClick(View view, int position);
+	}
 	
-	public NotesAdapter(Context context, MainActivity activity, ArrayList<FileItem> list) {
-		this.context = context;
-		this.list = list;
-		this.activity = activity;
+	private Context mContext;
+	private ArrayList<FileItem> mFileItems;
+	
+	public NotesAdapter(Context context, ArrayList<FileItem> fileItems) {
+		mContext = context;
+		mFileItems = fileItems;
+	}
+	
+	public void setFileItems(ArrayList<FileItem> fileItems) {
+		mFileItems = fileItems;
 	}
 	
 	@NonNull
 	@Override
 	public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View itemView = inflater.inflate(R.layout.notes_item, parent, false);
-		return new NotesViewHolder(itemView);
+		return new NotesAdapter.NotesViewHolder(itemView);
 	}
 	
 	@Override
 	public void onBindViewHolder(@NonNull NotesViewHolder holder, final int position) {
-		holder.folderName.setText(list.get(position).getName());
-		holder.deleteButton.setText("Delete");
-		holder.openButton.setText("Open");
-		holder.deleteButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				File file = new File(list.get(position).getPath());
-				list.remove(position);
-				file.delete();
-				notifyItemRemoved(position);
-			}
-		});
-		holder.openButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				if (list.get(position).getType().equals(FileItem.FileType.FILE_TYPE_FOLDER)) {
-					FileItem FileDes = new FileItem(list.get(position).getPath(), list.get(position).getName(), FileItem.FileType.FILE_TYPE_FOLDER);
-					Bundle bundle = new Bundle();
-					bundle.putParcelable("FileDes", FileDes);
-					activity.mNavController.navigate(R.id.basicNotesFragment, bundle);
-				} else {
-				}
-			}
-		});
+		Log.d(TAG, "onBindViewHolder: starts");
+		
+		FileItem fileItem = mFileItems.get(position);
+		holder.fileName.setText(fileItem.getName());
 	}
 	
 	@Override
 	public int getItemCount() {
-		return list.size();
+		return mFileItems.size();
 	}
 	
 	static class NotesViewHolder extends RecyclerView.ViewHolder {
 		
-		TextView folderName;
-		Button deleteButton;
-		Button openButton;
+		TextView fileName;
 		
 		public NotesViewHolder(View view) {
 			super(view);
-			folderName = view.findViewById(R.id.folder_name);
-			deleteButton = view.findViewById(R.id.delete_button);
-			openButton = view.findViewById(R.id.open_button);
+			
+			fileName = view.findViewById(R.id.file_name);
+		}
+	}
+	
+	public static class NotesItemTouchListener implements RecyclerView.OnItemTouchListener {
+		private NotesClickListener mNotesClickListener;
+		private GestureDetector mGestureDetector;
+		
+		public NotesItemTouchListener(Context context, final RecyclerView recyclerView, final NotesClickListener notesClickListener) {
+			this.mNotesClickListener = notesClickListener;
+			this.mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+				@Override
+				public boolean onSingleTapUp(MotionEvent e) {
+					return true;
+				}
+				
+				@Override
+				public void onLongPress(MotionEvent e) {
+					View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+					if (child != null && notesClickListener != null) {
+						notesClickListener.onLongClick(child, recyclerView.getChildAdapterPosition(child));
+					}
+				}
+			});
+		}
+		
+		@Override
+		public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+			View child = rv.findChildViewUnder(e.getX(), e.getY());
+			if (child != null && mNotesClickListener != null && mGestureDetector.onTouchEvent(e)) {
+				mNotesClickListener.onClick(child, rv.getChildAdapterPosition(child));
+			}
+			
+			return false;
+		}
+		
+		@Override
+		public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+		
+		}
+		
+		@Override
+		public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+		
 		}
 	}
 }
